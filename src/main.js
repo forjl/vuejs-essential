d43a3e64412cd39f1888f4ff6eab2f01e9424d41
forjl <748409412@qq.com>
2018-12-23 21:24:38 +0800

    map bug

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import router from './router'
+import './directives'
+import './components'
+// 引入 store/index.js 的默认值
+import store from './store'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
+  //注入store
+  store,
   components: { App },
   template: '<App/>'
 })
